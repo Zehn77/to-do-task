97aa56eac7d175f88f73e0e5da2e9488df1679f3
Zehn77 <zekhn777@gmail.com>
2025-05-07 23:40:59 +0500

    feat: implement date for each card

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent } from "react";
 import { Todo, Action } from "@/store/todoReducer";
+import { formatDateTime } from "@/helper/date";
 
 type CheckboxProps = {
   task: Todo;
@@ -19,14 +20,22 @@ export const Checkbox = ({ task, dispatch }: CheckboxProps) => {
         type="checkbox"
         checked={task.done}
         onChange={handleCheckboxChange}
-        className="w-4 h-4 bg-gray-100 border-gray-300 rounded-sm dark:bg-gray-700 dark:border-gray-600"
+        className="min-w-4 min-h-4 bg-gray-100 border-gray-300 rounded-sm dark:bg-gray-700 dark:border-gray-600"
       />
       <label
         htmlFor={String(task.id)}
         className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
       >
-        <span className={`${task.done ? "line-through" : ""}`}>
+        <span className={`${task.done ? "line-through text-gray-400" : ""}`}>
           {task.title}
+        </span>
+        <br />
+        <span
+          className={`text-[12px] ${
+            task.done ? " text-gray-400 line-through" : ""
+          }`}
+        >
+          {formatDateTime(task.date)}
         </span>
       </label>
     </div>
